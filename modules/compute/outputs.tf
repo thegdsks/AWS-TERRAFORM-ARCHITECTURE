174ac93bf5a9a31c0ab93b7c0c21bd877599d0fb
GDS K S <39922405+gagandua078@users.noreply.github.com>
2024-04-05 21:02:26 -0500

    Update outputs.tf

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,11 @@
+output "instance1_public_ip" {
+  value = aws_instance.instance1.public_ip
+}
 
+output "instance2_public_ip" {
+  value = aws_instance.instance2.public_ip
+}
+
+output "web_sg" {
+  value = aws_security_group.web.id
+}
