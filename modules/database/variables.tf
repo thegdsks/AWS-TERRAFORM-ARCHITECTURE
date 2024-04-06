c04049ba1707231197adb0dd22d7ea323ebafa3f
GDS K S <39922405+gagandua078@users.noreply.github.com>
2024-04-05 21:03:49 -0500

    Update variables.tf

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,1 +1,25 @@
+variable "private_subnets" {
+  description = "A list of private subnet IDs for the RDS instance"
+  type        = list(string)
+}
 
+variable "vpc_id" {
+  description = "The VPC ID."
+}
+
+variable "web_sg" {
+  description = "The security group ID for the web servers."
+}
+
+variable "db_instance_class" {
+  description = "The instance type of the RDS instance."
+  default     = "db.t3.micro"
+}
+
+variable "db_username" {
+  description = "Username for the RDS database."
+}
+
+variable "db_password" {
+  description = "Password for the RDS database."
+}
