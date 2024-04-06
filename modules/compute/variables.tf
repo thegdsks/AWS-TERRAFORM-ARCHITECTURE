a68ec12b5c6079655677e2cfb1e2cac0a11c7f66
GDS K S <39922405+gagandua078@users.noreply.github.com>
2024-04-05 21:02:04 -0500

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,1 +1,29 @@
+variable "instance1_name" {
+  description = "Name for the first EC2 instance"
+}
 
+variable "instance2_name" {
+  description = "Name for the second EC2 instance"
+}
+
+variable "public_subnets" {
+  description = "A list of public subnet IDs where EC2 instances will be launched"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "The VPC ID."
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the instances."
+}
+
+variable "instance_type" {
+  description = "The instance type of the EC2 instance."
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance."
+}
