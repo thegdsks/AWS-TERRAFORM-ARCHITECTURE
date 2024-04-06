8b1672b00584f7248647b8f4276cc76b2c04ca13
GDS K S <39922405+gagandua078@users.noreply.github.com>
2024-04-05 21:04:59 -0500

    Update variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,1 +1,18 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+}
 
+variable "public_subnet_cidrs" {
+  description = "A list of CIDR blocks for the public subnets."
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "A list of CIDR blocks for the private subnets."
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones in the region."
+  type        = list(string)
+}
